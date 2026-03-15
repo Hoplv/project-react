6ee3a72212556e4e9a37e745ea4eb040e70063e6
hoplv <vanhop110196@gmail.com>
2026-03-15 21:29:31 +0700

    Sửa header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>React Tuhoc.cc</h1>
         <p>
           Học React - Khám phá cách xây dựng ứng dụng linh hoạt, hiện đại, và
-          đầy sáng tạo năm 2100!
+          đầy sáng tạo năm 2050!
         </p>
       </header>
     </>
